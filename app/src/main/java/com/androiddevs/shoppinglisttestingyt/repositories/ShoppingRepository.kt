f6541843a7250849272a14465367c764ec9374aa
Amauri Guzmán <amauri.guzman@dacodes.com.mx>
2022-02-07 19:10:19 -0600

    Added repositories

---
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/repositories/ShoppingRepository.kt
@@ -0,0 +1,23 @@
+package com.androiddevs.shoppinglisttestingyt.repositories
+
+import androidx.lifecycle.LiveData
+import com.androiddevs.shoppinglisttestingyt.data.local.ShoppingItem
+import com.androiddevs.shoppinglisttestingyt.data.remote.responses.ImageResponse
+import com.androiddevs.shoppinglisttestingyt.other.Resource
+
+/**
+ * Created by Amalip on 2/7/2022.
+ */
+
+interface ShoppingRepository {
+
+    suspend fun insertShoppingItem(shoppingItem: ShoppingItem)
+
+    suspend fun deleteShoppingItem(shoppingItem: ShoppingItem)
+
+    fun observeAllShoppingItems(): LiveData<List<ShoppingItem>>
+
+    fun observeTotalPrice(): LiveData<Float>
+
+    suspend fun searchForImage(imageQuery: String): Resource<ImageResponse>
+}
