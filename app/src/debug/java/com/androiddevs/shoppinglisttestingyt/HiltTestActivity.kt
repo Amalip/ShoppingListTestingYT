da347ed139d0bbafcc960b43ebd06e0d130771ff
Amauri Guzmán <amauri.guzman@dacodes.com.mx>
2022-02-09 11:08:21 -0600

    Added support for fragment testing with Hilt (with error)

---
+++ app/src/debug/java/com/androiddevs/shoppinglisttestingyt/HiltTestActivity.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.shoppinglisttestingyt
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Created by Amalip on 2/8/2022.
+ */
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
